fe676858857757b6ac902b16c9acdf511349a9ae
LepakBoy <fikrinadzif35@gmail.com>
2021-11-23 21:44:10 +0700

    struktur folder

---
+++ components/modules/FooterComponent.js
@@ -0,0 +1,10 @@
+export default function FooterComponent() {
+  return (
+    <footer className="footer">
+      <div className="footer-class ">
+        <div className="footer-class-content">warna putih</div>
+        aowkawokawokawokaokawokaowk
+      </div>
+    </footer>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    URL_BACKEND: "http://localhost:3001",
+  },
+};
