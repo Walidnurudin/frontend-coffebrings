c09229acc80c195a9b258a218b2e68b32f0fb8a5
LepakBoy <fikrinadzif35@gmail.com>
2021-11-25 18:43:14 +0700

    slicing home page

---
+++ components/molecules/MenuHomeComponent.js
@@ -0,0 +1,372 @@
+/* eslint-disable @next/next/no-img-element */
+export default function MenuHomeComponent() {
+  return (
+    <>
+      <div className="menu-header d-flex justify-content-between p-4">
+        <div className="food-category-list selected-category">
+          Favorite Product
+        </div>
+        <div className="food-category-list">Coffee</div>
+        <div className="food-category-list">Non Coffee</div>
+        <div className="food-category-list">Foods</div>
+        <div className="food-category-list">Add-on</div>
+      </div>
+      <div
+        className="
+                menu-list
+                text-center
+                d-flex
+                justify-content-start
+                flex-wrap
+              "
+      >
+        {/* <!-- map menu-item-list dari sini --> */}
+        <div className="card-list-menu-item p-4 mt-3">
+          {/* <!-- kondisional isAdmin --> */}
+          <div
+            className="
+                    edit-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/pencil.png" alt="edit" />
+          </div>
+          <div
+            className="
+                    delete-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/trash 1.png" alt="delete" />
+          </div>
+          {/* <!--  ==================== --> */}
+          <img
+            src="/assets/food-img/food-2.png"
+            className="menu-item-img"
+            alt="pecel"
+          />
+          <div className="menu-item-name mt-2">summer fride rice</div>
+          <div className="menu-item-price">IDR 35.000</div>
+        </div>
+        {/* <!-- map menu-item-list sampe sini --> */}
+        <div className="card-list-menu-item p-4 mt-3">
+          {/* <!-- kondisional isAdmin --> */}
+          <div
+            className="
+                    edit-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/pencil.png" alt="edit" />
+          </div>
+          <div
+            className="
+                    delete-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/trash 1.png" alt="delete" />
+          </div>
+          {/* <!--  ==================== --> */}
+          <img
+            src="/assets/food-img/food-2.png"
+            className="menu-item-img"
+            alt="pecel"
+          />
+          <div className="menu-item-name mt-2">summer fride rice</div>
+          <div className="menu-item-price">IDR 35.000</div>
+        </div>
+        <div className="card-list-menu-item p-4 mt-3">
+          {/* <!-- kondisional isAdmin --> */}
+          <div
+            className="
+                    edit-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/pencil.png" alt="edit" />
+          </div>
+          <div
+            className="
+                    delete-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/trash 1.png" alt="delete" />
+          </div>
+          {/* <!--  ==================== --> */}
+          <img
+            src="/assets/food-img/food-2.png"
+            className="menu-item-img"
+            alt="pecel"
+          />
+          <div className="menu-item-name mt-2">summer fride rice</div>
+          <div className="menu-item-price">IDR 35.000</div>
+        </div>
+        <div className="card-list-menu-item p-4 mt-3">
+          {/* <!-- kondisional isAdmin --> */}
+          <div
+            className="
+                    edit-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/pencil.png" alt="edit" />
+          </div>
+          <div
+            className="
+                    delete-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/trash 1.png" alt="delete" />
+          </div>
+          {/* <!--  ==================== --> */}
+          <img
+            src="/assets/food-img/food-2.png"
+            className="menu-item-img"
+            alt="pecel"
+          />
+          <div className="menu-item-name mt-2">summer fride rice</div>
+          <div className="menu-item-price">IDR 35.000</div>
+        </div>
+        <div className="card-list-menu-item p-4 mt-3">
+          {/* <!-- kondisional isAdmin --> */}
+          <div
+            className="
+                    edit-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/pencil.png" alt="edit" />
+          </div>
+          <div
+            className="
+                    delete-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/trash 1.png" alt="delete" />
+          </div>
+          {/* <!--  ==================== --> */}
+          <img
+            src="/assets/food-img/food-2.png"
+            className="menu-item-img"
+            alt="pecel"
+          />
+          <div className="menu-item-name mt-2">summer fride rice</div>
+          <div className="menu-item-price">IDR 35.000</div>
+        </div>
+        <div className="card-list-menu-item p-4 mt-3">
+          {/* <!-- kondisional isAdmin --> */}
+          <div
+            className="
+                    edit-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/pencil.png" alt="edit" />
+          </div>
+          <div
+            className="
+                    delete-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/trash 1.png" alt="delete" />
+          </div>
+          {/* <!--  ==================== --> */}
+          <img
+            src="/assets/food-img/food-2.png"
+            className="menu-item-img"
+            alt="pecel"
+          />
+          <div className="menu-item-name mt-2">summer fride rice</div>
+          <div className="menu-item-price">IDR 35.000</div>
+        </div>
+        <div className="card-list-menu-item p-4 mt-3">
+          {/* <!-- kondisional isAdmin --> */}
+          <div
+            className="
+                    edit-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/pencil.png" alt="edit" />
+          </div>
+          <div
+            className="
+                    delete-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/trash 1.png" alt="delete" />
+          </div>
+          {/* <!--  ==================== --> */}
+          <img
+            src="/assets/food-img/food-2.png"
+            className="menu-item-img"
+            alt="pecel"
+          />
+          <div className="menu-item-name mt-2">summer fride rice</div>
+          <div className="menu-item-price">IDR 35.000</div>
+        </div>
+        <div className="card-list-menu-item p-4 mt-3">
+          {/* <!-- kondisional isAdmin --> */}
+          <div
+            className="
+                    edit-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/pencil.png" alt="edit" />
+          </div>
+          <div
+            className="
+                    delete-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/trash 1.png" alt="delete" />
+          </div>
+          {/* <!--  ==================== --> */}
+          <img
+            src="/assets/food-img/food-2.png"
+            className="menu-item-img"
+            alt="pecel"
+          />
+          <div className="menu-item-name mt-2">summer fride rice</div>
+          <div className="menu-item-price">IDR 35.000</div>
+        </div>
+        <div className="card-list-menu-item p-4 mt-3">
+          {/* <!-- kondisional isAdmin --> */}
+          <div
+            className="
+                    edit-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/pencil.png" alt="edit" />
+          </div>
+          <div
+            className="
+                    delete-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/trash 1.png" alt="delete" />
+          </div>
+          {/* <!--  ==================== --> */}
+          <img
+            src="/assets/food-img/food-2.png"
+            className="menu-item-img"
+            alt="pecel"
+          />
+          <div className="menu-item-name mt-2">summer fride rice</div>
+          <div className="menu-item-price">IDR 35.000</div>
+        </div>
+        <div className="card-list-menu-item p-4 mt-3">
+          {/* <!-- kondisional isAdmin --> */}
+          <div
+            className="
+                    edit-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/pencil.png" alt="edit" />
+          </div>
+          <div
+            className="
+                    delete-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/trash 1.png" alt="delete" />
+          </div>
+          {/* <!--  ==================== --> */}
+          <img
+            src="/assets/food-img/food-2.png"
+            className="menu-item-img"
+            alt="pecel"
+          />
+          <div className="menu-item-name mt-2">summer fride rice</div>
+          <div className="menu-item-price">IDR 35.000</div>
+        </div>
+        <div className="card-list-menu-item p-4 mt-3">
+          {/* <!-- kondisional isAdmin --> */}
+          <div
+            className="
+                    edit-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/pencil.png" alt="edit" />
+          </div>
+          <div
+            className="
+                    delete-menu
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                  "
+          >
+            <img src="/assets/images/trash 1.png" alt="delete" />
+          </div>
+          {/* <!--  ==================== --> */}
+          <img
+            src="/assets/food-img/food-2.png"
+            className="menu-item-img"
+            alt="pecel"
+          />
+          <div className="menu-item-name mt-2">summer fride rice</div>
+          <div className="menu-item-price">IDR 35.000</div>
+        </div>
+      </div>
+      <button className="button-add-product w-100 mt-4 border-0">
+        Add new product
+      </button>
+    </>
+  );
+}
---
+++ components/molecules/PromoHomeComponent.js
@@ -0,0 +1,75 @@
+/* eslint-disable @next/next/no-img-element */
+export default function PromoHomeComponent() {
+  return (
+    <>
+      <div className="promo-header px-5">
+        <div className="promo-title text-center">Promo Today</div>
+        <div className="promo-desc text-center px-4 mt-2">
+          Coupons will be updated every weeks. Check them out!
+        </div>
+      </div>
+      <div className="promo-list mt-3 px-3">
+        {/* <!-- map list card promo dari sini --> */}
+        <div className="promo-list-card bg-green d-flex p-1 py-1 px-2 mt-4">
+          <img
+            src="/assets/images/promo-card-1.png"
+            alt=""
+            className="promo-list-card-img"
+          />
+          <div className="promo-list-card-content ps-2 pt-2 px-4">
+            <div className="promo-card-header">HAPPY MOTHER’S DAY!</div>
+            <div className="promo-card-desc">
+              Get one of our favorite menu for free!
+            </div>
+          </div>
+        </div>
+        {/* <!-- map list card promo sampe sini --> */}
+        <div className="promo-list-card bg-yellow d-flex p-1 py-1 px-2 mt-4">
+          <img
+            src="/assets/images/promo-card-2.png"
+            alt=""
+            className="promo-list-card-img"
+          />
+          <div className="promo-list-card-content ps-2 pt-2 px-4">
+            <div className="promo-card-header">HAPPY MOTHER’S DAY!</div>
+            <div className="promo-card-desc">
+              Get one of our favorite menu for free!
+            </div>
+          </div>
+        </div>
+        <div className="promo-list-card bg-brown d-flex p-1 py-1 px-2 mt-4">
+          <img
+            src="/assets/images/promo-card-3.png"
+            alt=""
+            className="promo-list-card-img"
+          />
+          <div className="promo-list-card-content ps-2 pt-2 px-4">
+            <div className="promo-card-header">HAPPY MOTHER’S DAY!</div>
+            <div className="promo-card-desc">
+              Get one of our favorite menu for free!
+            </div>
+          </div>
+        </div>
+        <div className="promo-list-card bg-green d-flex p-1 py-1 px-2 mt-4">
+          <img
+            src="/assets/images/promo-card-1.png"
+            alt=""
+            className="promo-list-card-img"
+          />
+          <div className="promo-list-card-content ps-2 pt-2 px-4">
+            <div className="promo-card-header">HAPPY MOTHER’S DAY!</div>
+            <div className="promo-card-desc">
+              Get one of our favorite menu for free!
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="promo-btn-select px-3">
+        <button className="btn-apply-coupon w-100 mt-3 border-0 py-3">
+          Apply coupon
+          {/* <!-- admin = Add new promo --> */}
+        </button>
+      </div>
+    </>
+  );
+}
