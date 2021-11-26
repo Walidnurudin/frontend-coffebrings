b1b66d99accf93081ca502d6b1d1d3592147e334
Walidnurudin <walidnurudin47@gmail.com>
2021-11-26 17:31:26 +0700

    Integrasi: Dashboard get filter by daily & monthly

--- pages/admin/dashboard/index.js
+++ pages/admin/dashboard/index.js
@@ -1,7 +1,99 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { HeaderComponent, FooterComponent } from "components/modules";
+import { useRouter } from "next/router";
+import { getDataCookie } from "middleware/authorizationPage";
+import { Line } from "react-chartjs-2";
+import Chart from "chart.js/auto";
+import axios from "utils/axios";
+
+export async function getServerSideProps(context) {
+  const dataCookie = await getDataCookie(context);
+
+  if (!dataCookie.isLogin) {
+    return {
+      redirect: {
+        destination: "/auth/login",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+}
 
 function Dashboard() {
+  const router = useRouter();
+
+  const [data, setData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: "# of data",
+        fill: false,
+        backgroundColor: "#6a4029",
+        borderColor: "#ffba33",
+        data: [],
+      },
+    ],
+  });
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  const [filter, setFilter] = useState("monthly");
+
+  const getDashboard = () => {
+    axios
+      .get(`${process.env.URL_BACKEND}/dashboard?filter=${filter}`)
+      .then((res) => {
+        const result = res.data.data;
+        router.push(`/admin/dashboard?filter=${filter}`);
+
+        let labelDashboard = [];
+        let dataDashboard = [];
+
+        filter === "daily"
+          ? result?.map((item) => {
+              labelDashboard.push(item.day);
+              dataDashboard.push(item.total);
+            })
+          : result?.map((item) => {
+              labelDashboard.push(item.month);
+              dataDashboard.push(item.total);
+            });
+
+        setData({
+          labels: labelDashboard,
+          datasets: [
+            {
+              label: "# of data",
+              fill: false,
+              backgroundColor: "#6a4029",
+              borderColor: "#ffba33",
+              data: dataDashboard,
+            },
+          ],
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getDashboard();
+  }, []);
+
+  useEffect(() => {
+    getDashboard();
+  }, [filter]);
+
   return (
     <>
       <HeaderComponent />
@@ -18,6 +110,7 @@ function Dashboard() {
                 type="radio"
                 name="flexRadioDefault"
                 id="flexRadioDefault1"
+                onChange={() => setFilter("daily")}
               />
               <label className="form-check-label" htmlFor="flexRadioDefault1">
                 Daily
@@ -30,7 +123,7 @@ function Dashboard() {
                 type="radio"
                 name="flexRadioDefault"
                 id="flexRadioDefault2"
-                checked
+                onChange={() => setFilter("weekly")}
               />
               <label className="form-check-label" htmlFor="flexRadioDefault2">
                 Weekly
@@ -43,7 +136,7 @@ function Dashboard() {
                 type="radio"
                 name="flexRadioDefault"
                 id="flexRadioDefault2"
-                checked
+                onChange={() => setFilter("monthly")}
               />
               <label className="form-check-label" htmlFor="flexRadioDefault2">
                 Monthly
@@ -53,9 +146,11 @@ function Dashboard() {
 
           <div className="dashboard__page--chart mt-4">
             <h5 className="rubik-700">Monthly Report</h5>
-            <span className="text-secondary rubik-400">Last 9 months</span>
+            {/* <span className="text-secondary rubik-400">Last 9 months</span> */}
 
-            <div>chartjs</div>
+            <div>
+              <Line data={data} options={options} />
+            </div>
           </div>
 
           <div className="text-center">
