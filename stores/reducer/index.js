178b43b200dc7396c529bfc062301bb06f97d01d
Walidnurudin <walidnurudin47@gmail.com>
2021-11-28 00:31:25 +0700

    cart data local

--- stores/reducer/index.js
+++ stores/reducer/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import dataUserById from "./dataUser";
 import product from "./allProduct";
 import promo from "./promo";
+import addCart from "./addCart";
 
 export default combineReducers({
   product,
   promo,
   dataUserById,
+  addCart,
 });
