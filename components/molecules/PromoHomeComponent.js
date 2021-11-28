02cc1a58b664a3e5e7f5c93bbd72da59ea661cb5
LepakBoy <fikrinadzif35@gmail.com>
2021-11-28 14:42:55 +0700

    pull

--- components/molecules/PromoHomeComponent.js
+++ components/molecules/PromoHomeComponent.js
@@ -67,45 +67,49 @@ export default function PromoHomeComponent() {
       </div>
       <div className="promo-list mt-3 px-3">
         {/* <!-- map list card promo dari sini --> */}
-        {promo?.map((item) => (
-          <div
-            onClick={
-              userRole === "user" ? () => handleSelectedPromo(item.id) : null
-            }
-            className="promo-list-card bg-green d-flex p-1 py-1 px-2 mt-4 "
-            key={item.id}
-          >
-            <img
-              src="/assets/images/promo-card-1.png"
-              alt=""
-              className="promo-list-card-img"
-            />
-            <div className="promo-list-card-content ps-2 pt-2 px-4">
-              <div className="promo-card-header">{item.name}</div>
-              <div className="promo-card-desc">{item.description}</div>
+        {promo.map ? (
+          (item) => (
+            <div
+              onClick={
+                userRole === "user" ? () => handleSelectedPromo(item.id) : null
+              }
+              className="promo-list-card bg-green d-flex p-1 py-1 px-2 mt-4 "
+              key={item.id}
+            >
+              <img
+                src="/assets/images/promo-card-1.png"
+                alt=""
+                className="promo-list-card-img"
+              />
+              <div className="promo-list-card-content ps-2 pt-2 px-4">
+                <div className="promo-card-header">{item.name}</div>
+                <div className="promo-card-desc">{item.description}</div>
+              </div>
+              {user.role === "admin" ? (
+                <>
+                  <div
+                    onClick={() => toAddPromo(item.id)}
+                    className="edit-promo-menu  d-flex
+                    align-items-center
+                    justify-content-center "
+                  >
+                    <img src="/assets/images/pencil.png" alt="edit" />
+                  </div>
+                  <div
+                    onClick={() => showDelete(item.id)}
+                    className="delete-promo-menu  d-flex
+                    align-items-center
+                    justify-content-center "
+                  >
+                    <img src="/assets/images/trash 1.png" alt="edit" />
+                  </div>
+                </>
+              ) : null}
             </div>
-            {user.role === "admin" ? (
-              <>
-                <div
-                  onClick={() => toAddPromo(item.id)}
-                  className="edit-promo-menu  d-flex
-                    align-items-center
-                    justify-content-center "
-                >
-                  <img src="/assets/images/pencil.png" alt="edit" />
-                </div>
-                <div
-                  onClick={() => showDelete(item.id)}
-                  className="delete-promo-menu  d-flex
-                    align-items-center
-                    justify-content-center "
-                >
-                  <img src="/assets/images/trash 1.png" alt="edit" />
-                </div>
-              </>
-            ) : null}
-          </div>
-        ))}
+          )
+        ) : (
+          <h1>No promo found</h1>
+        )}
       </div>
       <div className="promo-btn-select px-3">
         <button
